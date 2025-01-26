e2c81b6e3f8d441fa715cc26316f2825ba371c78
shrimann23 <shrima.narasimhan@gmail.com>
2025-01-25 16:13:22 -0800

    merged app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import Login from './Login.jsx'
-import './styles.css';
-import { NewWorkFlowFormFillout } from "./form-fillout.jsx"
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Login from './Login.jsx';
+import Dashboard from './Dashboard.jsx'; // Assuming you have a Dashboard component
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./styles.css";
-import Dashboard from './Dashboard.jsx';
-
+import { NewWorkFlowFormFillout } from "./form-fillout.jsx"
 
 function App() {
   return (
+    <div className="App">
+      <header className="App-header">
+      </header>
       <Routes>
-        <Route path="createTutorial" element={< NewWorkFlowFormFillout />}/>
+        <Route path="/login" element={<Login user={user} />} />
+        <Route path="/" element={<Login user={user} />} />
         <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+        <Route path="/createTutorial" element={<NewWorkFlowFormFillout />} />
+      </Routes>
       
+    </div>
   );
 };
 
